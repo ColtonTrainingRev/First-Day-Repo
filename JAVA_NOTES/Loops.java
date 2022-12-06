eafe6b7dabde146871ec33df14cbdf975b3ceea2
ColtonTrainingRev <colton367@revature.net>
2022-12-06 13:09:52 -0800

    loops and operators

--- JAVA_NOTES/Loops.java
+++ JAVA_NOTES/Loops.java
@@ -9,5 +9,31 @@ public class Loops {
                 System.out.println("bop");
             }
         }
+
+        // storing numbers in arrays
+        int[] numbers = {1, 3, 5, 9, 15, 34, 73, 8,12345};
+        //to access contents of an array, we use index positions from 0 to sizeof(array) - 1
+        for(int x = 0; x < numbers.length; ++x){
+            if(numbers[x] % 2 == 0){
+                System.out.println("x is even");
+            } else {
+                System.out.println("x is odd");
+            }
+        }
+
+        //while loops
+        int x = 0;
+        while(x <= 10){
+            System.out.println(x);
+            ++x;
+        }
+
+        //do while loops
+        x = 0;
+        do{
+            System.out.println(x);
+            ++x;
+        } while(x <= 10);
+
     }
 }
